b11fa5601b07ace0b47742bd8e8075b0dc6557fe
naila <naila.pineda09@gmail.com>
2022-11-30 05:39:03 +0000

    fixed json file & loaded data in

--- src/components/ColorPalette.js
+++ src/components/ColorPalette.js
@@ -2,5 +2,21 @@ import { useState, useEffect } from "react";
 //import { Grid, Container } from "@mui/material";
 
 export default function ColorPalette() {
-  return <div>hi</div>;
+  const [rowColor, setRowColor] = useState([]);
+
+  useEffect(() => {
+    fetch("data/data.json")
+      .then((result) => result.json())
+      .then((data) => {
+        console.log(data);
+        //store data
+        setRowColor(data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <div>hi</div>
+    </div>
+  );
 }
