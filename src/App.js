ab47b99da8c3ef459aa264e69aa36c7980906c7d
npineda0 <97705502+npineda0@users.noreply.github.com>
2022-11-29 23:16:39 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import ColorPalette from "./components/ColorPalette";
+
+export default function App() {
+  return (
+    <div className="App">
+      <ColorPalette />
+    </div>
+  );
+}
---
+++ src/components/ColorPalette.js
@@ -0,0 +1,6 @@
+import { useState, useEffect } from "react";
+//import { Grid, Container } from "@mui/material";
+
+export default function ColorPalette() {
+  return <div>hi</div>;
+}
